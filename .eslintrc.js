7df474b4df6d6fc0f0ba201e63d7646dac0704cf
Adam Lui 刘展鹏 <adamlui@protonmail.com>
2023-05-03 00:43:52 -0700

    Allowed all escape chars, updated `parserOptions.ecmaVersion`

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,12 @@ module.exports  = {
         'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
         'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
         'no-constant-condition': 'off', // allow constant conditions
-        'no-empty': 'off' // allow empty blocks
+        'no-empty': 'off', // allow empty blocks
+        'no-useless-escape': 'off' // allow all escape chars because ESLint sucks at detecting useless ones
     },
     'globals': { 'chatgpt': true, 'global_module': true, '$': true, 'GM_cookie': true },
     'parserOptions': {
-        'ecmaVersion': 2020,
+        'ecmaVersion': 2022,
         'sourceType': 'script'
     },
     'env': { 'browser': true, 'node': true, 'es6': true, 'greasemonkey': true }
