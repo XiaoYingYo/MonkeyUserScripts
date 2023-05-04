c90b7310d28371428957bcdd22ad81902f421556
XiaoYingYo <759852125@qq.com>
2023-05-04 19:19:16 +0800

    update

--- chatgpt/clear-chat-history/clear-chat-history.js
+++ chatgpt/clear-chat-history/clear-chat-history.js
@@ -60,14 +60,132 @@
 var global_module = window['global_module'];
 var globalVariable = new Map();
 
-(async () => { 
+async function InitSvg() {
+    return new Promise(async (resolve) => {
+        let Svg = GM_getValue('clearSvg', []);
+        if (Svg.length !== 0) {
+            globalVariable.set('clearSvg', Svg);
+            resolve(true);
+            return;
+        }
+        let menuButton = document.querySelector('button[id^="headlessui-menu-button-"]');
+        menuButton.click();
+        let menuitems = [];
+        await new Promise((resolve) => {
+            let Timer = setInterval(() => {
+                menuitems = document.querySelectorAll('a[role="menuitem"]');
+                if (menuitems.length < 4) {
+                    return;
+                }
+                clearInterval(Timer);
+                resolve();
+            }, 100);
+        });
+        let menuitem = menuitems[1];
+        if (menuitem.name === 1) {
+            return;
+        }
+        let svg = menuitem.querySelector('svg');
+        globalVariable.set('clearSvg', []);
+        globalVariable.get('clearSvg').push(svg.outerHTML);
+        menuitem.click();
+        setTimeout(() => {
+            svg = menuitem.querySelector('svg');
+            globalVariable.get('clearSvg').push(svg.outerHTML);
+            menuitem.name = 1;
+            menuitem.remove();
+            menuButton.click();
+            GM_setValue('clearSvg', globalVariable.get('clearSvg'));
+            resolve(true);
+        }, 100);
+    });
+}
+
+function clearChats() {
+    let url = '/backend-api/conversations';
+    let method = 'PATCH';
+    let headers = {
+        Authorization: 'Bearer ' + globalVariable.get('accessToken'),
+        'Content-Type': 'application/json'
+    };
+    let body = { is_visible: false };
+    (async () => {
+        await globalVariable.get('Fetch')(url, { method, headers, body: JSON.stringify(body) });
+    })();
+}
+
+function createOrShowClearButton(Show = null) {
+    if (document.getElementById('_clearButton_') != null) {
+        if (Show != null) document.getElementById('_clearButton_').style.display = Show;
+        return;
+    }
+    let border = document.querySelectorAll('div[class^="border-"]');
+    border = border[border.length - 1];
+    let div = document.createElement('div');
+    div.id = '_clearButton_';
+    let className = border.childNodes[0].className;
+    div.className = className;
+    border.insertBefore(div, border.childNodes[0]);
+    (async function () {
+        if (globalVariable.get('clearSvg') == null) {
+            if (!(await InitSvg())) {
+                return;
+            }
+        }
+        div.innerHTML = globalVariable.get('clearSvg')[0] + 'Clear Conversations';
+        div.name = 0;
+        div.addEventListener('click', function () {
+            let title = 'Clear Conversations';
+            if (div.name === 0) {
+                title = 'Confirm ' + title;
+                div.name = 1;
+            } else {
+                div.name = 0;
+                clearChats();
+            }
+            div.innerHTML = globalVariable.get('clearSvg')[div.name] + title;
+        });
+    })();
+}
+
+(async () => {
     unsafeWindow['__hookRequest__'].FetchCallback.add('/api/auth/session', (_object, period) => {
-        if (period !== 'done') { 
+        if (period !== 'done') {
             return;
         }
         let json = JSON.parse(_object.text);
         let accessToken = json.accessToken;
-        localStorage.setItem('chatgpt.js.org.accessToken', accessToken);
+        localStorage.setItem('ChatGPT.accessToken', accessToken);
         globalVariable.set('accessToken', accessToken);
     });
+    unsafeWindow['__hookRequest__'].FetchCallback.add('/backend-api/conversations', (_object, period) => {
+        if (period !== 'done') {
+            return;
+        }
+        let url = _object.args[0];
+        if (url.indexOf('?') == -1) {
+            return;
+        }
+        let json = JSON.parse(_object.text);
+        if (json.total === 0) {
+            let title = '{_reserveHistory_}';
+            json.total = 0;
+            let time = new Date().toISOString();
+            json.items = [{ id: '', title, create_time: time, update_time: time }];
+            (async () => {
+                let rH = await global_module.waitForElement('div:contains("' + title + '")[class*="text-ellipsis"]', null, null, 100, -1);
+                rH = rH.eq(0);
+                rH.parent().hide();
+                await InitSvg();
+                createOrShowClearButton();
+            })();
+        }
+        _object.text = JSON.stringify(json);
+        return _object;
+    });
+    let newChat = await global_module.waitForElement('a[class*="items-center"]');
+    newChat = newChat.eq(0);
+    globalVariable.set('NewChatElement', newChat);
+    let chatHistoryElement = newChat.next().next();
+    globalVariable.set('NewChatHistoryElement', chatHistoryElement);
 })();
