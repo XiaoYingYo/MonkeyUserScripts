f4c4983507ba05a9a88ccfb83ac0b9f6fd88b04d
XiaoYingYo <759852125@qq.com>
2023-05-04 15:41:53 +0800

    update

--- chatgpt/chatgpt-heartbeat/chatgpt-heartbeat.js
+++ chatgpt/chatgpt-heartbeat/chatgpt-heartbeat.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://chat.openai.com/*
 // @match       *://freegpt.one/*
-// @version     XiaoYing_2023.05.16
+// @version     XiaoYing_2023.05.17
 // @grant       none
 // @run-at      document-body
 // @author      github.com @XiaoYingYo
--- qqmail/qq-mail-enhance.js
+++ qqmail/qq-mail-enhance.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://mail.qq.com/*
 // @grant       none
-// @version     XiaoYing_2023.05.16
+// @version     XiaoYing_2023.05.17
 // @grant       GM_info
 // @grant       GM_getValue
 // @grant       GM_setValue
