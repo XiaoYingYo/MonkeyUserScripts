ba469b656091943f2ed5701c5f6c6037d0459dde
XiaoYingYo <759852125@qq.com>
2023-05-05 17:04:57 +0800

    update

--- qqmail/qq-mail-enhance.js
+++ qqmail/qq-mail-enhance.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://mail.qq.com/*
 // @grant       none
-// @version     XiaoYing_2023.05.25.2
+// @version     XiaoYing_2023.05.25.3
 // @grant       GM_info
 // @grant       GM_getValue
 // @grant       GM_setValue
